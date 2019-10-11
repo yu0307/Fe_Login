63898defd29abeb04c185ee70166963941bc8d87
lucas <lucas.f.lu@nasa.gov>
2019-10-11 15:17:35 -0700

    Default to first tab when showing control window

--- src/assets/js/Fe_Login_usrOutlet.js
+++ src/assets/js/Fe_Login_usrOutlet.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#btn_usrCreate').click(function () {
         $('#control_CRUD .buttonSlot').html('<button class="btn btn-success usrSave" id="usrCreateUser">Create User</button>');
+        $('#User_Management_CRUD a:first').tab('show')
         showCRUD('User_Management');
     });
     $(document).on("click", '.usrSave', function (event) {
@@ -13,10 +14,11 @@ $(document).ready(function () {
             }
         });
     });
-    $(document).on('click','.users .user_img', function(e){
+    $(document).on('click', '.users .user_img', function (e) {
         $('#control_CRUD .buttonSlot').html('<button class="btn btn-success usrSave" id="usrUpdateUser">Update User</button>');
-        showCRUD('User_Management',true);
-        loadUsr($(this).parents('.users:first').attr('uid'),function(uid,data){
+        showCRUD('User_Management', true);
+        loadUsr($(this).parents('.users:first').attr('uid'), function (uid, data) {
+            $('#User_Management_CRUD a:first').tab('show')
             $('.User_Management #usr_ID').val(uid);
             $('.User_Management #usrName').val(data.name);
             $('.User_Management #email').val(data.email);
@@ -28,8 +30,8 @@ $(document).ready(function () {
     });
 
     $(document).on('click', '.users .usr_remove', function (e) {
-        if(confirm('Remove this User?')){
-            removeUsr($(this).parents('.users:first').attr('uid'), function (uid, data){
+        if (confirm('Remove this User?')) {
+            removeUsr($(this).parents('.users:first').attr('uid'), function (uid, data) {
                 Notify(data.message, (data.status !== undefined ? data.status : 'info'));
                 if (data.status === 'success') {
                     LoadList();

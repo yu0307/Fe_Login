7c822096375d0818f37cbbb1cec13a5ad4326784
Lucas <yu0307@gmail.com>
2020-05-14 05:04:12 -0700

    Update

--- src/http/controllers/FeSignupController.php
+++ src/http/controllers/FeSignupController.php
@@ -14,40 +14,16 @@ use feiron\fe_login\lib\events\UserCreated;
 
 class FeSignupController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-    use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
+    use RegistersUsers;
+    
     protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('Fe_Guest');
     }
 
-    /**
-     * Overriding registration form.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function showRegistrationForm()
     {
         return view('fe_login::LoginWindow',[
@@ -55,12 +31,6 @@ class FeSignupController extends Controller
         ]);
     }
 
-    /**
-     * Overriding registration request for the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function register(Request $request)
     {
         $validator= $this->validator($request->all());
@@ -80,12 +50,6 @@ class FeSignupController extends Controller
                         ?: ($request->ajax() ?  ['status' => 'success', 'message' => 'Registration Successfull'] : redirect($this->redirectPath()));
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -95,12 +59,6 @@ class FeSignupController extends Controller
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
     protected function create(array $data)
     {
         return fe_users::create([
